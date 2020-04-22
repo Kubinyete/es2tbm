6a94552418298b3a454cdb6c5dfef816b10e9660
MauricioBarbosa <mauriciobarboza6.1@gmail.com>
2020-04-22 19:22:12 -0300

    modified:   database/dados.sql       new file:   src/TBM/TBM/DAL/DALCargo.cs       new file:   src/TBM/TBM/DAL/DALUsuario.cs       new file:   src/TBM/TBM/Model/Cargo.cs       new file:   src/TBM/TBM/Model/Funcionario.cs       new file:   src/TBM/TBM/Model/PessoaFisica.cs       new file:   src/TBM/TBM/Model/Usuario.cs       new file:   src/TBM/TBM/config.json

---
+++ src/TBM/TBM/Model/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBM.Model
+{
+    class Usuario : PessoaFisica
+    {
+        private string _username;
+        private string _password;
+        private bool _ativado;
+        private Funcionario _funcionario;
+
+        public Usuario(string username,string password,bool ativado,Funcionario funcionario,
+            string cpf, string rg, string nome, DateTime data_nascimento, Endereco endereco) : 
+            base(cpf, rg, nome, data_nascimento, endereco)
+        {
+            Username = username;
+            Password = password;
+            Ativado = ativado;
+            Funcionario = funcionario;
+        }
+
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        public bool Ativado { get => _ativado; set => _ativado = value; }
+        internal Funcionario Funcionario { get => _funcionario; set => _funcionario = value; }
+    }
+}
